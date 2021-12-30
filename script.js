439643ae88b436540deb083eb3430190fba0a47e
Ramnec <79314663+Ramnec@users.noreply.github.com>
2021-12-30 18:44:33 +0500

    added result and result2 as global

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-
 var field1=document.getElementById("num1").value;
 var field2=document.getElementById("num2").value;
-
+var result;
+var result2;
 
 function calculate() {
 
-        var result = parseFloat(field1) * 0.621371192;
+        var result = parseInt(field1) * 0.621371192;
         document.getElementById("answer").innerHTML = field1 + " Km is " + Math.round(result) + " Miles";
   
 }
@@ -14,8 +13,8 @@ function calculate() {
 
 function calculateMiles() {
 
-    var result2 = parseFloat(field2) / 0.621371192;
-    document.getElementById("answer2").innerHTML = field2 + " Miles " + Math.round(result2) + " Km";
+    var result2 = parseInt(field2) / 0.621371192;
+    document.getElementById("answer2").innerHTML = field2 + " Miles is " + Math.round(result2) + " Km";
 
 }
 
